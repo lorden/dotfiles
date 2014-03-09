42bfda80d00a36e55b92641ecadac0de822a8115
Diego Lorden <djlorden@gmail.com>
2014-03-09 10:55:48 -0700

    clone vundle if missing

--- vim/install.sh
+++ vim/install.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+VUNDLE_DIR=$HOME/.vim/bundle/vundle
+if [ ! -d "$VUNDLE_DIR" ]; then
+    git clone https://github.com/gmarik/vundle.git $VUNDLE_DIR
+fi
 cp ~/.vimrc ~/.vimrc.bkp
 cp .vimrc ~/.vimrc
 cp -r .vim/* ~/.vim
