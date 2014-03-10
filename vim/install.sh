e85cdb462d5a3b08fa0c754142c0c5e42ea23c9f
diego <diego.lorden@livelovely.com>
2014-03-10 11:56:48 -0700

    better backup file name

--- vim/install.sh
+++ vim/install.sh
@@ -3,7 +3,7 @@ VUNDLE_DIR=$HOME/.vim/bundle/vundle
 if [ ! -d "$VUNDLE_DIR" ]; then
     git clone https://github.com/gmarik/vundle.git $VUNDLE_DIR
 fi
-cp ~/.vimrc ~/.vimrc.bkp
+cp ~/.vimrc ~/.vimrc.`date +%Y%m%d_%H%M%S`
 cp .vimrc ~/.vimrc
 cp -r .vim/* ~/.vim
 vim +BundleInstall +qall
